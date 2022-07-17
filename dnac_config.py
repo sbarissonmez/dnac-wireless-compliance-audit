4638517226f1b18269b0d62559cb0f94e51eecfa
Baris <s.barissonmez@gmail.com>
2022-07-17 19:28:34 +0300

    Initial commit

---
+++ dnac_config.py
@@ -0,0 +1,18 @@
+"""
+Sample Configuration
+DNAC_IP = "1.1.1.1"
+DNAC_PORT = 443
+USERNAME = "username"
+PASSWORD = "password"
+VERSION = "v1"
+DEVICE_IP = "2.2.2.2"
+PRODUCT_FAMILY = "Wireless Controller"
+"""
+DNAC_IP = ""
+DNAC_PORT = 443
+USERNAME = "username"
+PASSWORD = "password"
+VERSION = "v1"
+DEVICE_IP = "Device IP Address"
+PRODUCT_FAMILY = "Device Family - Any one from the list {Wireless Controller,Routers, Switches}"
+VERBOSE = False
